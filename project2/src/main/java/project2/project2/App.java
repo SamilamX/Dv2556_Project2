0671f51437d59febc40ccf068b59f87303aceeb5
LAPTOP-L7BN2IPQ\gaikt <gaikteng16@gmail.com>
2022-04-14 15:41:52 +0200

    App - new function printHi() Test - new JUnit Test for printHi()

--- project2/src/main/java/project2/project2/App.java
+++ project2/src/main/java/project2/project2/App.java
@@ -2,9 +2,15 @@ package project2.project2;
 
 public class App {
     
+	public void printHi() {
+		
+		System.out.println("Hello World");
+	
+	}
+	
     public static void main(String[] args) {
     	
-    	System.out.println("Hello World");
+    	printHi();
     	
     }
     
---
+++ project2/src/test/java/project2/project2/Test1.java
@@ -0,0 +1,21 @@
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class Test1 {
+
+	@Test
+	public void test1() {
+		// App test = new App();
+	
+		String actualOutput = printHi();
+		String expectedOutput = "Hello World";
+		
+		assertEquals(expectedOutput, actualOutput);
+
+	}
+
+}
