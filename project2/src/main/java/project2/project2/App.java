0d0159eeb7a3b4e5d5663ffa0ccf7338ea1ff8ce
Ramesh88806 <57959335+Ramesh88806@users.noreply.github.com>
2022-04-22 16:05:57 +0200

    updated sort function and count function By Ramesh

--- project2/src/main/java/project2/project2/App.java
+++ project2/src/main/java/project2/project2/App.java
@@ -2,24 +2,33 @@ package project2.project2;
 import java.util.Arrays;
 public class App {
     
-	static void printHi() {
+	
+	
+	static void sort() {
+		//committed by Ramesh
 		
-		System.out.println("Hello World!");
-        System.out.println("This is Ramesh!");
+		Arrays.sort(array);
+		System.out.println("Elements of array sorted in ascending order: ");
+		for (int i = 0; i<array.length;i++)
+		{
+		System.out.println(array[i]);
+		}	
 
 	
 	}
 	
+	static void count() {
+		int count = 0, num = 123456;
+
+	    for (; num != 0; num /= 10, ++count) {
+	    }
+
+	    System.out.println("Number of digits: " + count);
+	}
     public static void main(String[] args) {
-    int [] array = new int [] {90,23,5,109,12,22,67,34};
-	Arrays.sort(array);
-	System.out.println("Elements of array sorted in ascending order: ");
-	for (int i = 0; i<array.length;i++)
-	{
-	System.out.println(array[i]);
-	}	
-    	//hi sam
-    	printHi();
+    	int [] array = new int [] {90,23,5,109,12,22,67,34};
+        sort();
+    	count();
     	
     }
     
